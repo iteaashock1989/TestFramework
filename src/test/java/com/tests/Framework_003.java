9e0ef7c0edbe68affd369f3718258b9b589d9cd1
Ashok V <33321957+iteaashock1989@users.noreply.github.com>
2017-11-23 16:23:53 +0530

    v1.4.3

--- src/test/java/com/tests/Framework_003.java
+++ src/test/java/com/tests/Framework_003.java
@@ -6,8 +6,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.appModules.CheckOut_Action;
-import com.appModules.Confirmation_Action;
 import com.utils.Const;
 import com.utils.ExcelUtils;
 import com.utils.Log;
